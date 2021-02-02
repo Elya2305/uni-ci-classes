72c79b6830fb259d7eaefcf9725d81fda04f49cb
Elya <62761810+Elya2305@users.noreply.github.com>
2021-02-02 17:54:25 +0200

    test first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    double F;
+    cout << "Temperature: ";
+    cin >> a;
+    F = a * 9.0 / 5.0 + 32.0;
+    cout << F << endl;
+    return 0;
+}
