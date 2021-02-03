56d581766c8901ca69ad92089b244259eb21cff7
Elya <62761810+Elya2305@users.noreply.github.com>
2021-02-03 08:59:08 +0200

    Task 0.3 b)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+double task_03_b(double x) {
+    return pow(x, 4) + pow(x, 3) + pow(x, 2) + x + 1;
+}
+
+double task_03_b_v_2(double x) {
+    return (pow(x, 5) - 1) / (x - 1);
+}
+
 int main() {
-    int a;
-    double F;
-    cout << "Temperature: ";
-    cin >> a;
-    F = a * 9.0 / 5.0 + 32.0;
-    cout << F << endl;
+    float x;
+    cout << "X=";
+    cin >> x;
+
+    cout << "Result: " << task_03_b(x) << "\n";
+    cout << "Result: " << task_03_b_v_2(x);
+
     return 0;
 }
