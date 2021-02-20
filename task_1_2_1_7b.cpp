5bebbbdf82e0b47523f6cc48c1c75856ece3eed5
Elya <62761810+Elya2305@users.noreply.github.com>
2021-02-20 20:13:37 +0200

    Task 1.2.1.7b updated

--- task_1_2_1_7b.cpp
+++ task_1_2_1_7b.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 
-float min_positive(float x1, float x2, float x3) {
-    float x = x1 + x2 + x3 + 200;
+int min_positive(float x1, float x2, float x3) {
+    float x;
+    int k = 3;
 
-    while (x < 0) {
+    while (x3 < 0) {
+        x = x1 + x2 + x3 + 200;
         x1 = x2;
         x2 = x3;
         x3 = x;
-        x = x1 + x2 + x3 + 200;
     }
-    return x;
+    cout << "X = " << x << "\n";
+    return k;
 }
 
 int main() {
-
     float x = -99;
-
-    cout << min_positive(x, x, x);
+    int k = min_positive(x, x, x);
+    
+    cout << "k = " << k << "\n";
 }
